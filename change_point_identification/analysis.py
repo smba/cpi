3706c48c1febc003f6d8ea83837f37037d3a9639
Stefan Mühlbauer <stefan@hubble.medien.uni-weimar.de>
2020-01-08 11:04:22 +0100

    jo

--- change_point_identification/analysis.py
+++ change_point_identification/analysis.py
@@ -1,7 +1,6 @@
 import numpy as np
 import itertools
-import matplotlib.pyplot as plt
-
+from change_point_identification.synthesis import PerformanceHistorySynthesizer
 
 def find_runs(value, a):
     # Create an array that is 1 where a is `value`, and pad each end with an extra 0.
@@ -18,13 +17,19 @@ class Transformation:
     
     @staticmethod
     def transform_single(n_revisions: int, obs: dict, epsilon = 0.01):
-        observed = sorted(obs.keys())
-        ys = np.zeros(n_revisions)
+        '''
         
-        # Tuning of Delta
+        :param n_revisions:
+        :param obs:
+        :param epsilon:
+        '''
+        observed = sorted(obs.keys())
+        
+        # tune window size delta
         yss = {}
         dd = []
-        for delta in range(n_revisions // 3):        
+        for delta in range(n_revisions // 4):        
+            ys = np.zeros(n_revisions)
             for a, b in itertools.combinations(observed, 2):
                 if np.abs(b - a) < delta and np.abs(obs[b] - obs[a]) > epsilon:
                     ys[a:b] += 1.0
@@ -32,12 +37,23 @@ class Transformation:
             
             dd.append(len(find_runs(0, ys)))
         
-        plt.plot(range(n_revisions // 3), dd)
-        plt.axvline(np.argsort(dd, kind="stable")[-1], color="dodgerblue")
-        plt.show()
+        # optimal window size (trade-off between information used and number of segments)
+        opt = np.argsort(dd, kind="stable")[-1]
+        
+        return yss[opt]
             
-
+class GroupSamplingEstimator():
+    
+    def __init__(self, synthesizer: PerformanceHistorySynthesizer):
+        self.synthesizer = synthesizer
+        
+    def initialize(self, sample_size: int, sample_rate: float = 0.05, p_select: float = 0.5):
+        n_features = self.synthesizer.__n_features
+        
+        
+        
 dd = np.zeros(1000)
 dd[400:] += 1
-dd[700:] += 2
+dd[800:] += 2
 s = Transformation().transform_single(1000, {i: dd[i] for i in np.random.choice(np.arange(1000), size=50, replace=False)})
+
